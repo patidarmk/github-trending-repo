40072dd098818968c0ac89dfe71e2a9ad8d9cd08
unknown <patidarmukesh123@gmail.com>
2023-06-18 22:16:38 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="https://hros-assignment.netlify.app/home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
